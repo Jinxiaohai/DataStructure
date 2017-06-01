45c501ad853dbba224694f34e16187ce3725a7e9
xiaohai <xiaohaijin@outlook.com>
2017-06-01 13:39:11 +0800

    console

---
+++ C++Primer/console/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <cstddef>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <random>
+#include "control.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  Error<string> error;
+  Warning<string> warning;
+  Prompt<string> prompt;
+
+  error(cout) << "sAL;DJFLS;ADJF;LSKJDF;LKASJDFL;JASL;DKF" << error.end() << endl;
+  return 0;
+}
